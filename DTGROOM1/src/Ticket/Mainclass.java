caae669135b54b2c391f76ab299c6c7c83045cfd
RAJARSHI-DHAR <dharrajarshi92@gmail.com>
2017-04-16 23:33:36 +0530

    Add files via upload

---
+++ DTGROOM1/src/Ticket/Mainclass.java
@@ -0,0 +1,13 @@
+package Ticket;
+import java.util.*;
+public class Mainclass  {
+
+	public static void main(String[] args) throws Exception
+	{
+	
+		Operation operation= new Operation();
+         operation.show();
+	
+	}
+
+}
